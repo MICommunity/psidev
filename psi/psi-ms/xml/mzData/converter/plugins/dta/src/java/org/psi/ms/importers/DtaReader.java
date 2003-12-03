314e6c2cf5cf8b22a3f304ea2340cf25e5bf4525
krunte <krunte>
2003-12-03 17:49:38 +0000

    Added the first ImporterI implementation, that is a plugin!

---
+++ psi/psi-ms/xml/mzData/converter/plugins/dta/src/java/org/psi/ms/importers/DtaReader.java
@@ -0,0 +1,184 @@
+/**
+ * $Id: DtaReader.java,v 1.1 2003/12/03 17:49:38 krunte Exp $
+ *
+ * Created by IntelliJ IDEA.
+ * User: krunte
+ * Date: Jul 23, 2003
+ * Time: 11:30:26 AM
+ */
+package org.psi.ms.importers;
+
+import org.psi.ms.model.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Reads in a given .dta file and adds it to a given MzData.
+ *
+ * @author krunte
+ */
+public class DtaReader {
+    public final static int BASE64 = 1;
+    public final static int XML_ELEMENTS = 2;
+    public final static int DTA = 1;
+    public final static int ZTA = 2;
+    private int fileType = -1;
+
+    public DtaReader() {
+    }
+
+    public Acquisition addAcquisitions(String filepath) throws IOException {
+        /*
+         * ..zta files and .dta files are ordered (.zta are first then .dta) and
+         * for triple-play experiments there is one each with identical file
+         * names except for the extension (as you implemented in your code).
+         *
+         * The scan id should be incremented from 1 to n.
+         *
+         * The file name can be read backward from the file extension:
+         * The last number before the file extension (before the last '.') is
+         *  the charge state - this can be obtained from the first line of the
+         *  file.
+         * Then there is an '_'
+         * Then there is a decimal number representing acquisition time.ÊThis
+         *  number can be repeated in our 'range' as both start and stop.
+         * Then there is an '_'
+         * Then there is the scan number of the precursor ion (this is messy
+         *  since the actual scan number of the .dta is not available from the
+         *  .dta...)
+         * Then there is an '_' and everything else is the sample name
+         *  (including any "_" or ".")
+         *
+         * Also, the MS level is always 2 for .dta and 1 for .zta.
+         *
+         * The pattern of the filepath is:
+         * (sample name)_(sample name)_(sample name)_(scan number).(acquisition time)_(charge state).(zta|dta)
+         */
+        File path = new File(filepath);
+        String filename = path.getName();
+        Pattern pattern = Pattern.compile("(.+)_(\\d+).(\\d+)_(\\d+).(zta|dta)");
+        Matcher matcher = pattern.matcher(filename);
+        // Todo: What is this scan-number supposed to do?
+        int scanNumber = -1;
+        int acquisitionTime = -1;
+        if (matcher.matches()) {
+            String scanNumberString = matcher.group(2);
+            scanNumber = new Integer(scanNumberString).intValue();
+            String acquisitionTimeString = matcher.group(3);
+            acquisitionTime = new Integer(acquisitionTimeString).intValue();
+        }
+
+        int msLevel = 2;
+        if (filepath.endsWith(".zta")) {
+            fileType = ZTA;
+            msLevel = 1;
+        } else {
+            fileType = DTA;
+        }
+
+        AcqDesc acqDesc = new AcqDesc();
+
+        AcqSettings acqSettings = new AcqSettings();
+        acqDesc.setAcqSettings(acqSettings);
+        if (fileType == DTA) {
+            acqSettings.setSpecType(AcqSettings.SpecType.DISCRETE);
+        }
+        if (fileType == ZTA) {
+            acqSettings.setSpecType(AcqSettings.SpecType.CONTINUOUS);
+        }
+
+        // At this stage we don't know yet the range or list of acquisition numbers
+        AcqSpecification acqSpecification = new AcqSpecification();
+        Range range = new Range();
+        range.setStart(acquisitionTime);
+        range.setEnd(acquisitionTime);
+        acqSpecification.setRange(range);
+        //Todo: this is user-supplied information?
+        acqSpecification.setMethod(AcqSpecification.Method.AVERAGE);
+        acqSettings.setAcqSpecification(acqSpecification);
+
+        InstrumentAcqSettings instrument2 = new InstrumentAcqSettings();
+        if (fileType == ZTA) {
+            instrument2.setAcqType(InstrumentAcqSettings.AcqType.ZOOM);
+            instrument2.setMsLevel(msLevel);
+        } else {
+            instrument2.setAcqType(InstrumentAcqSettings.AcqType.FULL);
+            instrument2.setMsLevel(msLevel);
+        }
+        acqSettings.setInstrument(instrument2);
+
+        FileReader fileReader = new FileReader(filepath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        // Extracting the peptide mass and the peptide charge stored in
+        // in the first line of the .dta file.
+        String firstLine = bufferedReader.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(firstLine);
+        String peptideMassString = stringTokenizer.nextToken();
+        String peptideChargeString = stringTokenizer.nextToken();
+        Float peptideMass = new Float(peptideMassString);
+        Integer peptideCharge = new Integer(peptideChargeString);
+
+        PrecursorList precursorList = acqDesc.getPrecursorList();
+        if (precursorList == null) {
+            precursorList = new PrecursorList();
+            acqDesc.setPrecursorList(precursorList);
+        }
+
+        Precursor precursor = new Precursor();
+        Ion ion = new Ion();
+        ion.setMz(peptideMass.floatValue());
+        ion.setCharge(peptideCharge.intValue());
+        precursor.setIon(ion);
+        Activation activation = new Activation();
+        activation.setMethod(Activation.Method.OTHER);
+        precursor.setActivation(activation);
+        precursor.setMsLevel(msLevel);
+        precursorList.addPrecursor(precursor);
+
+        Acquisition acquisition = new Acquisition();
+        // Now create the mass and intensity arrays
+        addXMLArrays(acquisition, bufferedReader);
+
+        // Set the description of the acquisition
+        acquisition.setAcqDesc(acqDesc);
+
+        return acquisition;
+    }
+
+
+    private int addXMLArrays(Acquisition acquisition, BufferedReader bufferedReader) {
+        StringTokenizer stringTokenizer;
+        RawDataType mzArray = new RawDataType();
+        acquisition.setMzArray(mzArray);
+        RawDataType intenArray = new RawDataType();
+        acquisition.setIntenArray(intenArray);
+
+        String line;
+        int size = 0;
+        try {
+            while ((line = bufferedReader.readLine()) != null) {
+                size++;
+                stringTokenizer = new StringTokenizer(line);
+                String mzString = stringTokenizer.nextToken();
+                String intensityString = stringTokenizer.nextToken();
+                Float mz = new Float(mzString);
+                Float intensity = new Float(intensityString);
+                mzArray.addFloat(mz.floatValue());
+                intenArray.addFloat(intensity.floatValue());
+            }
+        } catch (IOException e) {
+            // EOF
+        }
+
+        mzArray.setLength(size);
+        intenArray.setLength(size);
+        return size;
+    }
+}
---
+++ psi/psi-ms/xml/mzData/converter/plugins/dta/src/java/org/psi/ms/importers/DtaSetImporter.java
@@ -0,0 +1,295 @@
+/**
+ * $Id: DtaSetImporter.java,v 1.1 2003/12/03 17:49:38 krunte Exp $
+ *
+ * Created by IntelliJ IDEA.
+ * User: krunte
+ * Date: Jul 23, 2003
+ * Time: 5:24:22 PM
+ */
+package org.psi.ms.importers;
+
+import org.psi.ms.helper.PsiMsConverterException;
+import org.psi.ms.helper.SuffixFileFilter;
+import org.psi.ms.helper.Utils;
+import org.psi.ms.model.MzData;
+import org.psi.ms.model.Desc;
+import org.psi.ms.model.Acquisition;
+import org.psi.ms.model.Admin;
+import org.psi.ms.xml.MzDataWriter;
+import org.psi.ms.importers.DtaReader;
+import org.psi.ms.converter.ImporterI;
+import org.psi.ms.converter.ParseListener;
+import org.psi.ms.converter.ProvidedDataItem;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+/**
+ * Reads in all .dta/.zta from a given directory and converts them
+ * into a single peaklist XML document. Per default the arrays are
+ * stored in the space-saving base64 format.
+ *
+ * @author krunte
+ */
+public class DtaSetImporter implements ImporterI {
+
+    private DtaReader dtaReader;
+
+    private SortedSet dtaFileSet = null;
+    private Iterator dtaIterator = null;
+    private SortedSet ztaFileSet = null;
+    private Iterator ztaIterator = null;
+
+    /**
+     * This defines which file type (.dta or .zta) should be returned
+     * with the next call of {@link #getNextAcquisition() getNextAcquisition}.
+     */
+    private int currentFileType = -1;
+
+    /**
+     * The ParseListener for providing feedback to the (graphical) user interface.
+     */
+    private ParseListener parseListener = null;
+
+    private Vector providedData = null;
+
+    public DtaSetImporter() {
+        dtaReader = new DtaReader();
+    }
+
+    // Todo: swing package relies on this method still.
+    public void convertDirectory(String sourceDirname, String outputFilename, MzData mzData, MzDataWriter.OutputType outputType, ParseListener listener) throws PsiMsConverterException, IOException {
+        Desc desc = initialize(new File(sourceDirname), listener);
+        String sampleName = desc.getAdmin().getSampleName();
+        mzData.getDesc().getAdmin().setSampleName(sampleName);
+
+        MzDataWriter mzDataWriter = new MzDataWriter(new File(outputFilename));
+        mzDataWriter.setOutputType(outputType);
+
+        int acquisitionCount = this.getAcquisitionCount();
+        mzDataWriter.initialize(mzData, acquisitionCount);
+        parseListener.setMax(acquisitionCount);
+        while (this.hasMoreAcquisitions()) {
+            Acquisition acquisition = this.getNextAcquisition();
+            parseListener.increment();
+            mzDataWriter.marshall(acquisition);
+        }
+        mzDataWriter.finish();
+    }
+
+    /**
+     * Initializes a Desc(ription) object from the provide source file or directory.
+     * @param source The source file or directory.
+     * @param listener The ParseListener for providing feedback to the (graphical) user interface.
+     * @throws org.psi.ms.helper.PsiMsConverterException in case of a conversion error.
+     * @throws java.io.IOException in case of an file/directory access error.
+     * @return A Desc object filled with meta-information harvested from the source.
+     */
+    public Desc initialize(File source,
+                           ParseListener listener) throws PsiMsConverterException, IOException {
+        parseListener = listener;
+        Desc desc = new Desc();
+        if (source.isDirectory()) {
+            File[] dtaFiles = source.listFiles(new SuffixFileFilter(".dta"));
+            File[] ztaFiles = source.listFiles(new SuffixFileFilter(".zta"));
+            dtaFileSet = Utils.fileArrayToTreeSet(dtaFiles);
+            ztaFileSet = Utils.fileArrayToTreeSet(ztaFiles);
+
+            currentFileType = DtaReader.DTA;
+            if (dtaFileSet.size() == 0) {
+                if (ztaFileSet.size() > 0) {
+                    currentFileType = DtaReader.ZTA;
+                } else {
+                    throw new PsiMsConverterException("No .dta/.zta files found!");
+                }
+            }
+            dtaIterator = dtaFileSet.iterator();
+            ztaIterator = ztaFileSet.iterator();
+
+            String filename = dtaFiles[0].getName();
+            Pattern pattern = Pattern.compile("(.+)_(\\d+).(\\d+)_(\\d+).(zta|dta)");
+            Matcher matcher = pattern.matcher(filename);
+            if (matcher.matches()) {
+                Admin admin = desc.getAdmin();
+                String sampleName = admin.getSampleName();
+                if (sampleName == null || sampleName.equals("")) {
+                    sampleName = matcher.group(1);
+                    admin.setSampleName(sampleName);
+                }
+            }
+        } else if (source.isFile()) {
+            dtaFileSet = new TreeSet();
+            ztaFileSet = new TreeSet();
+            String filename = source.getName();
+            Pattern pattern = Pattern.compile("(.+)_(\\d+).(\\d+)_(\\d+).(zta|dta)");
+            Matcher matcher = pattern.matcher(filename);
+            if (matcher.matches()) {
+                Admin admin = desc.getAdmin();
+                String sampleName = admin.getSampleName();
+                if (sampleName == null || sampleName.equals("")) {
+                    sampleName = matcher.group(1);
+                    admin.setSampleName(sampleName);
+                }
+                String suffix = matcher.group(5);
+                if (suffix.equals("dta")) {
+                    dtaFileSet.add(filename);
+                    currentFileType = DtaReader.DTA;
+                } else if (suffix.equals("zta")) {
+                    ztaFileSet.add(filename);
+                    currentFileType = DtaReader.ZTA;
+                }
+            }
+        }
+        return desc;
+    }
+
+    /**
+     * Returns a list of {@link org.psi.ms.converter.ProvidedDataItem ProvidedDataItem} objects.
+     * Tells the (graphical) user interface which pieces of information it does not
+     * need to request from the user.
+     * @return a list of class objects (must be classes of the package org.psi.ms.model).
+     * @see org.psi.ms.converter.ProvidedDataItem
+     */
+    public List getProvidedData() {
+        if (providedData == null) {
+            providedData = new Vector();
+            providedData.add(ProvidedDataItem.DESC_ADMIN_SAMPLENAME);
+        }
+        return providedData;
+    }
+
+    /**
+     * Returns whether single file input is supported or not.
+     * @return whether single file input is supported or not.
+     */
+    public boolean supportsFile() {
+        return true;
+    }
+
+    /**
+     * Returns whether directory input is supported or not.
+     * @return whether directory input is supported or not.
+     */
+    public boolean supportsDirectory() {
+        return true;
+    }
+
+    /**
+     * Returns whether the File object points to a valid input source for this plugin or not.
+     * @param file File object pointing to an input source. Can be a single file or directory.
+     * @return whether the File object points to a valid input source for this plugin or not.
+     */
+    public boolean isSupportedInputFormat(File file) {
+        Pattern pattern = Pattern.compile("(.+)_(\\d+).(\\d+)_(\\d+).(zta|dta)");
+        if (file.isDirectory()) {
+            File[] files = file.listFiles(new SuffixFileFilter(".dta"));
+            Matcher matcher = pattern.matcher(files[0].getName());
+            return matcher.matches();
+        } else if (file.isFile()) {
+            Matcher matcher = pattern.matcher(file.getName());
+            return matcher.matches();
+        }
+        return false;
+    }
+
+    /**
+     * Returns the number of acquisitions found in the source file/directory
+     * provided by {@link #initialize(java.io.File, org.psi.ms.converter.ParseListener) initialize}.
+     * N.B.: Depending on the input format, it might necessary that Converter
+     * implementation needs to scan through the file/directory.
+     * @return the number of acquisitions found in the source file/directory
+     * provided by {@link #initialize(java.io.File, org.psi.ms.converter.ParseListener) initialize}.
+     * @throws org.psi.ms.helper.PsiMsConverterException if {@link #initialize(java.io.File, org.psi.ms.converter.ParseListener) initialize} was not called.
+     * @throws java.io.IOException in case of an file/directory access error.
+     */
+    public int getAcquisitionCount() throws PsiMsConverterException, IOException {
+        if (dtaFileSet == null || ztaFileSet == null)
+            throw new PsiMsConverterException("Importer was not initialized!");
+        return dtaFileSet.size() + ztaFileSet.size();
+    }
+
+    /**
+     * Returns whether the source file/directory provided by
+     * {@link #initialize(java.io.File, org.psi.ms.converter.ParseListener) initialize}
+     * contains any more Acquisitions.
+     * @throws org.psi.ms.helper.PsiMsConverterException if {@link #initialize(java.io.File, org.psi.ms.converter.ParseListener) initialize} was not called.
+     * @return whether the source file/directory contains any more Acquistions.
+     * @see #initialize
+     */
+    public boolean hasMoreAcquisitions() throws PsiMsConverterException {
+        if (dtaIterator == null || ztaIterator == null)
+            throw new PsiMsConverterException("Importer was not initialized!");
+        // This is bit trickier to implement as if there are .zta files,
+        // we have to alternate between returning .dta and .zta
+        // acquisitions.
+        switch (currentFileType) {
+            case DtaReader.DTA:
+                return dtaIterator.hasNext();
+            case DtaReader.ZTA:
+                return ztaIterator.hasNext();
+            default:
+                throw new PsiMsConverterException("Importer was not initialized!");
+        }
+    }
+
+    /**
+     * Returns the next available Acquisition or null.
+     * @throws java.io.IOException in case of an file/directory access error.
+     * @return the next available Acquisition or null.
+     */
+    public Acquisition getNextAcquisition() throws IOException {
+        // This is bit trickier to implement as if there are .zta files,
+        // we have to alternate between returning .dta and .zta
+        // acquisitions.
+        Acquisition acquisition = null;
+        String filepath = null;
+        switch (currentFileType) {
+            case DtaReader.DTA:
+                filepath = (String) dtaIterator.next();
+                if (parseListener != null) {
+                    parseListener.setMessage("Converting file: " + filepath);
+                } else {
+                    System.out.println("Converting file: " + filepath);
+                }
+                acquisition = dtaReader.addAcquisitions(filepath);
+                if (ztaFileSet.size() != 0)
+                    currentFileType = DtaReader.ZTA;
+                break;
+            case DtaReader.ZTA:
+                filepath = (String) ztaIterator.next();
+                if (parseListener != null) {
+                    parseListener.setMessage("Converting file: " + filepath);
+                } else {
+                    System.out.println("Converting file: " + filepath);
+                }
+                acquisition = dtaReader.addAcquisitions(filepath);
+                if (dtaFileSet.size() != 0)
+                    currentFileType = DtaReader.DTA;
+                break;
+        }
+        return acquisition;
+    }
+
+    /**
+     * Returns some information about the importer implementation.
+     * This can be displayed, for example, in a list of available importers.
+     * @return some information about the importer implementation.
+     * Todo: should this text appear in desc/test/dataProcessing/software?
+     */
+    public String getImporterDescription() {
+        return "This importer reads sets of or single .dta or .zta files.";
+    }
+
+    /**
+     * Returns the name of the importer implementation.
+     * This can be displayed, for example, in a list of available importers.
+     * @return some information about the importer implementation.
+     * Todo: should this text appear in desc/test/dataProcessing/software?
+     */
+    public String getImporterName() {
+        return ".dta/.zta importer";
+    }
+}
